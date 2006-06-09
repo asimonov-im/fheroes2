9a42ba4b049e89fe908e163a6014947fd766c659
afletdinov <afletdinov@4f904287-c015-0410-838d-95d3a2656228>
2006-06-09 04:33:06 +0000

    svn: revision 2
    git-svn-id: https://fheroes2.svn.sourceforge.net/svnroot/fheroes2/trunk@3 4f904287-c015-0410-838d-95d3a2656228

--- editor2/mp2maps.c
+++ editor2/mp2maps.c
@@ -1,3 +1,31 @@
+/*
+    freeHeroes2 engine
+    turn-based game engine (clone of Heroes Of the Might and Magic II)
+    Copyright (C) 2006
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    3DO, New World Computing, Heroes of Might and Magic, and the related
+    3DO logos are trademarks and or registered trademarks of 3DO Company.
+    All other trademarks belong to their respective owners.
+
+    Web: http://sourceforge.net/projects/fheroes2
+
+    Description:
+*/
+
 #include "mp2maps.h"
 #include "gamedefs.h"
 
