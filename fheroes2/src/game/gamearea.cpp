7aeca79e0a8c3c3cb31ac311327d2249fe169698
afletdinov <afletdinov@4f904287-c015-0410-838d-95d3a2656228>
2008-05-27 01:13:23 +0000

    fog algorithm
    git-svn-id: https://fheroes2.svn.sourceforge.net/svnroot/fheroes2/trunk@234 4f904287-c015-0410-838d-95d3a2656228

--- fheroes2/src/game/gamearea.cpp
+++ fheroes2/src/game/gamearea.cpp
@@ -403,27 +403,27 @@ void RedrawClopOrClofSpriteFog(const u16 dst_index, const u8 ox, const u8 oy)
 	bool revert = false;
 
 	// see ICN::CLOP32: sprite 6, 7, 8
-	if(around == (Direction::CENTER | Direction::TOP))
+	if(around & (Direction::CENTER | Direction::TOP) && !(around & (Direction::BOTTOM | Direction::LEFT | Direction::RIGHT)))
 	{ index = 6; revert = false; }
 	else
-	if(around == (Direction::CENTER | Direction::RIGHT))
+	if(around & (Direction::CENTER | Direction::RIGHT) && !(around & (Direction::TOP | Direction::BOTTOM | Direction::LEFT)))
 	{ index = 7; revert = false; }
 	else
-	if(around == (Direction::CENTER | Direction::LEFT))
+	if(around & (Direction::CENTER | Direction::LEFT) && !(around & (Direction::TOP | Direction::BOTTOM | Direction::RIGHT)))
 	{ index = 7; revert = true; }
 	else
-	if(around == (Direction::CENTER | Direction::BOTTOM))
+	if(around & (Direction::CENTER | Direction::BOTTOM) && !(around & (Direction::TOP | Direction::LEFT | Direction::RIGHT)))
 	{ index = 8; revert = false; }
 	else
 	// see ICN::CLOP32: sprite 9, 29
-	if(around == (Direction::CENTER | Direction::TOP | Direction::BOTTOM))
+	if(around & (Direction::CENTER | Direction::TOP | Direction::BOTTOM) && !(around & (Direction::LEFT | Direction::RIGHT)))
 	{ index = 9; revert = false; }
 	else
-	if(around == (Direction::CENTER | Direction::LEFT | Direction::RIGHT))
+	if(around & (Direction::CENTER | Direction::LEFT | Direction::RIGHT) && !(around & (Direction::TOP | Direction::BOTTOM)))
 	{ index = 29; revert = false; }
 	else
 	// see ICN::CLOP32: sprite 10
-	if(around == Direction::CENTER)
+	if((around & Direction::CENTER) && !(around & (Direction::TOP | Direction::BOTTOM | Direction::LEFT | Direction::RIGHT)))
 	{ index = 10; revert = false; }
 	else
 	// see ICN::CLOP32: sprite 15, 22
@@ -472,25 +472,109 @@ void RedrawClopOrClofSpriteFog(const u16 dst_index, const u8 ox, const u8 oy)
 	{ index = 14; revert = true; }
 	else
 	// see ICN::CLOP32: sprite 11, 12
-	if(around == (DIRECTION_ALL & (~(Direction::TOP_LEFT | DIRECTION_TOP_RIGHT_CORNER))) ||
-	    around == (DIRECTION_ALL & (~(Direction::BOTTOM_RIGHT | DIRECTION_TOP_RIGHT_CORNER))) ||
-	    around == (DIRECTION_ALL & (~(Direction::TOP_LEFT | Direction::BOTTOM_RIGHT | DIRECTION_TOP_RIGHT_CORNER))))
+	if(around & (Direction::CENTER | Direction::LEFT | Direction::BOTTOM_LEFT | Direction::BOTTOM) &&
+	 !(around & (Direction::TOP | Direction::TOP_RIGHT | Direction::RIGHT)))
 	{ index = 11; revert = false; }
 	else
-	if(around == (DIRECTION_ALL & (~(Direction::TOP_RIGHT | DIRECTION_TOP_LEFT_CORNER))) ||
-	    around == (DIRECTION_ALL & (~(Direction::BOTTOM_LEFT | DIRECTION_TOP_LEFT_CORNER))) ||
-	    around == (DIRECTION_ALL & (~(Direction::TOP_RIGHT | Direction::BOTTOM_LEFT | DIRECTION_TOP_LEFT_CORNER))))
+	if(around & (Direction::CENTER | Direction::RIGHT | Direction::BOTTOM_RIGHT | Direction::BOTTOM) &&
+	 !(around & (Direction::TOP | Direction::TOP_LEFT | Direction::LEFT)))
 	{ index = 11; revert = true; }
 	else
-	if(around == (DIRECTION_ALL & (~(Direction::TOP_RIGHT | DIRECTION_BOTTOM_RIGHT_CORNER))) ||
-	    around == (DIRECTION_ALL & (~(Direction::BOTTOM_LEFT | DIRECTION_BOTTOM_RIGHT_CORNER))) ||
-	    around == (DIRECTION_ALL & (~(Direction::TOP_RIGHT | Direction::BOTTOM_LEFT | DIRECTION_BOTTOM_RIGHT_CORNER))))
+	if(around & (Direction::CENTER | Direction::LEFT | Direction::TOP_LEFT | Direction::TOP) &&
+	 !(around & (Direction::BOTTOM | Direction::BOTTOM_RIGHT | Direction::RIGHT)))
 	{ index = 12; revert = false; }
 	else
-	if(around == (DIRECTION_ALL & (~(Direction::TOP_LEFT | DIRECTION_BOTTOM_LEFT_CORNER))) ||
-	    around == (DIRECTION_ALL & (~(Direction::BOTTOM_RIGHT | DIRECTION_BOTTOM_LEFT_CORNER))) ||
-	    around == (DIRECTION_ALL & (~(Direction::TOP_LEFT | Direction::BOTTOM_RIGHT | DIRECTION_BOTTOM_LEFT_CORNER))))
+	if(around & (Direction::CENTER | Direction::RIGHT | Direction::TOP_RIGHT | Direction::TOP) &&
+	 !(around & (Direction::BOTTOM | Direction::BOTTOM_LEFT | Direction::LEFT)))
 	{ index = 12; revert = true; }
+	else
+	// see ICN::CLOP32: sprite 19, 20, 22
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::RIGHT | Direction::TOP | Direction::TOP_LEFT | Direction::LEFT) &&
+	 !(around & (Direction::BOTTOM_LEFT | Direction::BOTTOM_RIGHT | Direction::TOP_RIGHT)))
+	{ index = 19; revert = false; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::RIGHT | Direction::TOP | Direction::TOP_RIGHT | Direction::LEFT) &&
+	 !(around & (Direction::BOTTOM_LEFT | Direction::BOTTOM_RIGHT | Direction::TOP_LEFT)))
+	{ index = 19; revert = true; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::RIGHT | Direction::TOP | Direction::LEFT | Direction::BOTTOM_LEFT) &&
+	 !(around & (Direction::TOP_RIGHT | Direction::BOTTOM_RIGHT | Direction::TOP_LEFT)))
+	{ index = 20; revert = false; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::RIGHT | Direction::TOP | Direction::LEFT | Direction::BOTTOM_RIGHT) &&
+	 !(around & (Direction::TOP_RIGHT | Direction::BOTTOM_LEFT | Direction::TOP_LEFT)))
+	{ index = 20; revert = true; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::RIGHT | Direction::TOP | Direction::LEFT) &&
+	 !(around & (Direction::TOP_RIGHT | Direction::BOTTOM_RIGHT | Direction::BOTTOM_LEFT | Direction::TOP_LEFT)))
+	{ index = 22; revert = false; }
+	else
+	// see ICN::CLOP32: sprite 24, 25, 26, 30
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::RIGHT | Direction::LEFT | Direction::BOTTOM_LEFT) &&
+	 !(around & (Direction::TOP | Direction::BOTTOM_RIGHT)))
+	{ index = 24; revert = false; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::RIGHT | Direction::LEFT | Direction::BOTTOM_RIGHT) &&
+	 !(around & (Direction::TOP | Direction::BOTTOM_LEFT)))
+	{ index = 24; revert = true; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::LEFT | Direction::TOP | Direction::TOP_LEFT) &&
+	 !(around & (Direction::RIGHT | Direction::BOTTOM_LEFT)))
+	{ index = 25; revert = false; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::RIGHT | Direction::TOP | Direction::TOP_RIGHT) &&
+	 !(around & (Direction::LEFT | Direction::BOTTOM_RIGHT)))
+	{ index = 25; revert = true; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::BOTTOM_LEFT | Direction::LEFT | Direction::TOP) &&
+	 !(around & (Direction::RIGHT | Direction::TOP_LEFT)))
+	{ index = 26; revert = false; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::BOTTOM_RIGHT | Direction::RIGHT | Direction::TOP) &&
+	 !(around & (Direction::LEFT | Direction::TOP_RIGHT)))
+	{ index = 26; revert = true; }
+	else
+	if(around & (Direction::CENTER | Direction::LEFT | Direction::TOP_LEFT | Direction::RIGHT | Direction::TOP) &&
+	 !(around & (Direction::BOTTOM | Direction::TOP_RIGHT)))
+	{ index = 30; revert = false; }
+	else
+	if(around & (Direction::CENTER | Direction::LEFT | Direction::TOP_RIGHT | Direction::RIGHT | Direction::TOP) &&
+	 !(around & (Direction::BOTTOM | Direction::TOP_LEFT)))
+	{ index = 30; revert = true; }
+	else
+	// see ICN::CLOP32: sprite 27, 28
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::LEFT) &&
+	 !(around & (Direction::TOP | Direction::TOP_RIGHT | Direction::RIGHT | Direction::BOTTOM_LEFT)))
+	{ index = 27; revert = false; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::RIGHT) &&
+	 !(around & (Direction::TOP | Direction::TOP_LEFT | Direction::LEFT | Direction::BOTTOM_RIGHT)))
+	{ index = 27; revert = true; }
+	else
+	if(around & (Direction::CENTER | Direction::LEFT | Direction::TOP) &&
+	 !(around & (Direction::TOP_LEFT | Direction::RIGHT | Direction::BOTTOM | Direction::BOTTOM_RIGHT)))
+	{ index = 28; revert = false; }
+	else
+	if(around & (Direction::CENTER | Direction::RIGHT | Direction::TOP) &&
+	 !(around & (Direction::TOP_RIGHT | Direction::LEFT | Direction::BOTTOM | Direction::BOTTOM_LEFT)))
+	{ index = 28; revert = true; }
+	else
+	// see ICN::CLOP32: sprite 31, 32, 33
+	if(around & (Direction::CENTER | Direction::TOP | Direction::LEFT | Direction::RIGHT) &&
+	 !(around & (Direction::BOTTOM | Direction::TOP_LEFT | Direction::TOP_RIGHT)))
+	{ index = 31; revert = false; }
+	else
+	if(around & (Direction::CENTER | Direction::TOP | Direction::BOTTOM | Direction::RIGHT) &&
+	 !(around & (Direction::LEFT | Direction::TOP_RIGHT | Direction::BOTTOM_RIGHT)))
+	{ index = 32; revert = false; }
+	else
+	if(around & (Direction::CENTER | Direction::TOP | Direction::BOTTOM | Direction::LEFT) &&
+	 !(around & (Direction::RIGHT | Direction::TOP_LEFT | Direction::BOTTOM_LEFT)))
+	{ index = 32; revert = true; }
+	else
+	if(around & (Direction::CENTER | Direction::BOTTOM | Direction::LEFT | Direction::RIGHT) &&
+	 !(around & (Direction::TOP | Direction::BOTTOM_LEFT | Direction::BOTTOM_RIGHT)))
+	{ index = 33; revert = false; }
 	// unknown
 	else
 	{
