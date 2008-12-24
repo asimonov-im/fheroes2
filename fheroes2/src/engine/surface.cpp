7b57ce98ac2abe746c182c68e91108ae9570d984
peter_lemenkov <peter_lemenkov@4f904287-c015-0410-838d-95d3a2656228>
2008-12-24 06:40:12 +0000

    another one header necessary for gcc4
    git-svn-id: https://fheroes2.svn.sourceforge.net/svnroot/fheroes2/trunk@807 4f904287-c015-0410-838d-95d3a2656228

--- fheroes2/src/engine/surface.cpp
+++ fheroes2/src/engine/surface.cpp
@@ -18,6 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <algorithm>
 #include "surface.h"
 #include "palette.h"
 #include "error.h"
