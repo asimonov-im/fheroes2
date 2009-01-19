c6288ab355f7e2000cfa9991065b44b7d61d1f02
afletdinov <afletdinov@4f904287-c015-0410-838d-95d3a2656228>
2009-01-19 02:48:34 +0000

    fix build for gcc 4.3
    git-svn-id: https://fheroes2.svn.sourceforge.net/svnroot/fheroes2/trunk@824 4f904287-c015-0410-838d-95d3a2656228

--- fheroes2/src/fheroes2/heroes/heroes_indicator.cpp
+++ fheroes2/src/fheroes2/heroes/heroes_indicator.cpp
@@ -18,6 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             * 
  ***************************************************************************/
 
+#include <cstdlib>
 #include <cmath>
 #include "agg.h"
 #include "luck.h"
