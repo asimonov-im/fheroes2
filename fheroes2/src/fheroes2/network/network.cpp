cd46aa35eb122aaf2e74f0b6ce9a27fdded5a84f
revvy <revvy@4f904287-c015-0410-838d-95d3a2656228>
2009-04-08 14:54:19 +0000

    More ArchLinux build fixes.  Patch by Ben Cropley.
    git-svn-id: https://fheroes2.svn.sourceforge.net/svnroot/fheroes2/trunk@937 4f904287-c015-0410-838d-95d3a2656228

--- fheroes2/src/fheroes2/network/network.cpp
+++ fheroes2/src/fheroes2/network/network.cpp
@@ -18,6 +18,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <cstdlib>
+
 #include "sdlnet.h"
 #include "settings.h"
 #include "server.h"
