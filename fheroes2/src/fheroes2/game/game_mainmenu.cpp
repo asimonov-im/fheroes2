23c1cd21220fbd0691adb55c4e1a3274e2499ec1
revvy <revvy@4f904287-c015-0410-838d-95d3a2656228>
2009-04-28 19:33:38 +0000

    Simplify main menu button code, change sprites to reflect original.
    git-svn-id: https://fheroes2.svn.sourceforge.net/svnroot/fheroes2/trunk@1032 4f904287-c015-0410-838d-95d3a2656228

--- fheroes2/src/fheroes2/game/game_mainmenu.cpp
+++ fheroes2/src/fheroes2/game/game_mainmenu.cpp
@@ -25,6 +25,12 @@
 #include "button.h"
 #include "game.h"
 
+#define NEWGAME_DEFAULT 1
+#define LOADGAME_DEFAULT 5
+#define HIGHSCORES_DEFAULT 9
+#define CREDITS_DEFAULT 13
+#define QUIT_DEFAULT 17
+
 Game::menu_t Game::MainMenu(void)
 {
     Mixer::Pause();
@@ -49,17 +55,17 @@ Game::menu_t Game::MainMenu(void)
 
     LocalEvent & le = LocalEvent::GetLocalEvent();
 
-    const Sprite & s1 = AGG::GetICN(ICN::BTNSHNGL, 0);
-    const Sprite & s2 = AGG::GetICN(ICN::BTNSHNGL, 4);
-    const Sprite & s3 = AGG::GetICN(ICN::BTNSHNGL, 8);
-    const Sprite & s4 = AGG::GetICN(ICN::BTNSHNGL, 12);
-    const Sprite & s5 = AGG::GetICN(ICN::BTNSHNGL, 16);
+    const Sprite & s1 = AGG::GetICN(ICN::BTNSHNGL, NEWGAME_DEFAULT);
+    const Sprite & s2 = AGG::GetICN(ICN::BTNSHNGL, LOADGAME_DEFAULT);
+    const Sprite & s3 = AGG::GetICN(ICN::BTNSHNGL, HIGHSCORES_DEFAULT);
+    const Sprite & s4 = AGG::GetICN(ICN::BTNSHNGL, CREDITS_DEFAULT);
+    const Sprite & s5 = AGG::GetICN(ICN::BTNSHNGL, QUIT_DEFAULT);
 
-    Button buttonNewGame(s1.x(), s1.y(), ICN::BTNSHNGL, 0, 3);
-    Button buttonLoadGame(s2.x(), s2.y(), ICN::BTNSHNGL, 4, 7);
-    Button buttonHighScores(s3.x(), s3.y(), ICN::BTNSHNGL, 8, 11);
-    Button buttonCredits(s4.x(), s4.y(), ICN::BTNSHNGL, 12, 15);
-    Button buttonQuit(s5.x(), s5.y(), ICN::BTNSHNGL, 16, 19);
+    Button buttonNewGame(s1.x(), s1.y(), ICN::BTNSHNGL, NEWGAME_DEFAULT, NEWGAME_DEFAULT + 2);
+    Button buttonLoadGame(s2.x(), s2.y(), ICN::BTNSHNGL, LOADGAME_DEFAULT, LOADGAME_DEFAULT + 2);
+    Button buttonHighScores(s3.x(), s3.y(), ICN::BTNSHNGL, HIGHSCORES_DEFAULT, HIGHSCORES_DEFAULT + 2);
+    Button buttonCredits(s4.x(), s4.y(), ICN::BTNSHNGL, CREDITS_DEFAULT, CREDITS_DEFAULT + 2);
+    Button buttonQuit(s5.x(), s5.y(), ICN::BTNSHNGL, QUIT_DEFAULT, QUIT_DEFAULT + 2);
 
     const Point lt_pt(0, 0);
 
@@ -80,146 +86,44 @@ Game::menu_t Game::MainMenu(void)
 
     u32 ticket = 0;
     u32 lantern_frame = 0;
-    
-    u8 trigger_anime1 = 0;
-    u8 trigger_anime2 = 4;
-    u8 trigger_anime3 = 8;
-    u8 trigger_anime4 = 12;
-    u8 trigger_anime5 = 16;
+
+    u8 buttonsFrames[5] = {
+        NEWGAME_DEFAULT,
+        LOADGAME_DEFAULT,
+        HIGHSCORES_DEFAULT,
+        CREDITS_DEFAULT,
+        QUIT_DEFAULT
+    };
+
+    Button *buttons[5] = {
+        &buttonNewGame,
+        &buttonLoadGame,
+        &buttonHighScores,
+        &buttonCredits,
+        &buttonQuit
+    };
 
     // mainmenu loop
     while(le.HandleEvents())
     {
-	le.MousePressLeft(buttonNewGame) ? buttonNewGame.PressDraw() : buttonNewGame.ReleaseDraw();
-	le.MousePressLeft(buttonLoadGame) ? buttonLoadGame.PressDraw() : buttonLoadGame.ReleaseDraw();
-	le.MousePressLeft(buttonHighScores) ? buttonHighScores.PressDraw() : buttonHighScores.ReleaseDraw();
-	le.MousePressLeft(buttonCredits) ? buttonCredits.PressDraw() : buttonCredits.ReleaseDraw();
-	le.MousePressLeft(buttonQuit) ? buttonQuit.PressDraw() : buttonQuit.ReleaseDraw();
-
-	// anime new button
-	if(le.MouseCursor(buttonNewGame))
-	{
-	    if(2 > trigger_anime1)
-	    {
-		DELAY(8);
-		cursor.Hide();
-		const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, ++trigger_anime1);
-		display.Blit(sprite, sprite.x(), sprite.y());
-		cursor.Show();
-		display.Flip();
-	    }
-	}
-	else
-	if(0 != trigger_anime1)
-	{
-	    trigger_anime1 = 0;
-
-	    cursor.Hide();
-	    const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, trigger_anime1);
-	    display.Blit(sprite, sprite.x(), sprite.y());
-	    cursor.Show();
-	    display.Flip();
-	}
-
-	// anime load button
-	if(le.MouseCursor(buttonLoadGame))
-	{
-	    if(6 > trigger_anime2)
-	    {
-		DELAY(8);
-		cursor.Hide();
-		const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, ++trigger_anime2);
-		display.Blit(sprite, sprite.x(), sprite.y());
-		cursor.Show();
-		display.Flip();
-	    }
-	}
-	else
-	if(4 != trigger_anime2)
-	{
-	    trigger_anime2 = 4;
-
-	    cursor.Hide();
-	    const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, trigger_anime2);
-	    display.Blit(sprite, sprite.x(), sprite.y());
-	    cursor.Show();
-	    display.Flip();
-	}
-
-	// anime scores button
-	if(le.MouseCursor(buttonHighScores))
-	{
-	    if(10 > trigger_anime3)
-	    {
-		DELAY(8);
-		cursor.Hide();
-		const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, ++trigger_anime3);
-		display.Blit(sprite, sprite.x(), sprite.y());
-		cursor.Show();
-		display.Flip();
-	    }
-	}
-	else
-	if(8 != trigger_anime3)
-	{
-	    trigger_anime3 = 8;
-
-	    cursor.Hide();
-	    const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, trigger_anime3);
-	    display.Blit(sprite, sprite.x(), sprite.y());
-	    cursor.Show();
-	    display.Flip();
-	}
-
-	// anime credits button
-	if(le.MouseCursor(buttonCredits))
-	{
-	    if(14 > trigger_anime4)
-	    {
-		DELAY(8);
-		cursor.Hide();
-		const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, ++trigger_anime4);
-		display.Blit(sprite, sprite.x(), sprite.y());
-		cursor.Show();
-		display.Flip();
-	    }
-	}
-	else
-	if(12 != trigger_anime4)
-	{
-	    trigger_anime4 = 12;
-
-	    cursor.Hide();
-	    const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, trigger_anime4);
-	    display.Blit(sprite, sprite.x(), sprite.y());
-	    cursor.Show();
-	    display.Flip();
-	}
-
-	// anime quit button
-	if(le.MouseCursor(buttonQuit))
-	{
-	    if(18 > trigger_anime5)
-	    {
-		DELAY(8);
-		cursor.Hide();
-		const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, ++trigger_anime5);
-		display.Blit(sprite, sprite.x(), sprite.y());
-		cursor.Show();
-		display.Flip();
-	    }
-	}
-	else
-	if(16 != trigger_anime5)
-	{
-	    trigger_anime5 = 16;
-
-	    cursor.Hide();
-	    const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, trigger_anime5);
-	    display.Blit(sprite, sprite.x(), sprite.y());
-	    cursor.Show();
-	    display.Flip();
-	}
+        for(u16 i = 0; i < sizeof(buttons) / sizeof(buttons[0]); i++)
+        {
+            if(le.MousePressLeft(*buttons[i]))
+                buttons[i]->PressDraw();
+            else
+            {
+                buttons[i]->ReleaseDraw();
+                u16 frame = buttonsFrames[i];
+                if(le.MouseCursor(*buttons[i]))
+                    frame++;
+                cursor.Hide();
+                const Sprite & sprite = AGG::GetICN(ICN::BTNSHNGL, frame);
+                display.Blit(sprite, sprite.x(), sprite.y());
+                cursor.Show();
+                display.Flip();
+            }
+        }
+    
 
 	if(le.KeyPress(KEY_n) || le.MouseClickLeft(buttonNewGame)) return NEWGAME;
 	else
