73a02be336f58354f68ff306981fedfbe56a9cc8
afletdinov <afletdinov@4f904287-c015-0410-838d-95d3a2656228>
2010-02-11 04:20:47 +0000

    update Rand::Get from manual(3)
    git-svn-id: https://fheroes2.svn.sourceforge.net/svnroot/fheroes2/trunk@1462 4f904287-c015-0410-838d-95d3a2656228

--- fheroes2/src/engine/rand.cpp
+++ fheroes2/src/engine/rand.cpp
@@ -29,7 +29,7 @@
 
 void Rand::Init(void){ std::srand((u32) std::time(0)); }
 
-u32 Rand::Get(u32 min, u32 max){ return max ? min + (std::rand() % (max - min + 1)) : std::rand() % (min + 1); }
+u32 Rand::Get(u32 min, u32 max){ return max ? min + Get(max - min) : static_cast<u32>((min + 1) * (std::rand() / (RAND_MAX + 1.0))); }
 
 Rand::Queue::Queue(u32 size)
 {
