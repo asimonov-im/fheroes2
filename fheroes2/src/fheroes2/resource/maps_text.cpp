8497060748acc6edee9b48342d88277e47c885fa
afletdinov <afletdinov@4f904287-c015-0410-838d-95d3a2656228>
2010-03-09 06:43:27 +0000

    update maps text
    git-svn-id: https://fheroes2.svn.sourceforge.net/svnroot/fheroes2/trunk@1586 4f904287-c015-0410-838d-95d3a2656228

--- fheroes2/src/fheroes2/resource/maps_text.cpp
+++ fheroes2/src/fheroes2/resource/maps_text.cpp
@@ -1,6 +1,7 @@
 /*
  * message strings
- * from original maps files
+ * from original maps files! (do not edit this)
+
  *
  * Part of the Free Heroes2 Engine:
  * http://sourceforge.net/projects/fheroes2
@@ -8,7 +9,7 @@
 
 #include "gamedefs.h"
 
-#ifdef MAPS_MESSAGES
+#ifdef 0
 
 const char* maps_messages[] = {
 _("425 bottles of beer on the wall, 425 bottles of beer! If one of those bottles should happen to fall, there'll be..."),
