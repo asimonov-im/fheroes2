3cd5c3efba6069c657f85c75d0017ee19d05e992
afletdinov <afletdinov@4f904287-c015-0410-838d-95d3a2656228>
2010-03-23 12:10:26 +0000

    fix build gcc 4.4
    git-svn-id: https://fheroes2.svn.sourceforge.net/svnroot/fheroes2/trunk@1694 4f904287-c015-0410-838d-95d3a2656228

--- fheroes2/src/fheroes2/system/settings.cpp
+++ fheroes2/src/fheroes2/system/settings.cpp
@@ -20,6 +20,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <algorithm>
 #include <fstream>
 #include <bitset>
 #include "maps.h"
