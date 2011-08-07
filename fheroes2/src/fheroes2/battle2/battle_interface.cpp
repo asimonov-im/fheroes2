7e47011ea5fdae383e4c82c381e35dd9ddb149ed
afletdinov <afletdinov@4f904287-c015-0410-838d-95d3a2656228>
2011-08-07 23:49:24 +0000

    battle: fixed necromancer hero sprite
    git-svn-id: https://fheroes2.svn.sourceforge.net/svnroot/fheroes2/trunk@2439 4f904287-c015-0410-838d-95d3a2656228

--- fheroes2/src/fheroes2/battle2/battle_interface.cpp
+++ fheroes2/src/fheroes2/battle2/battle_interface.cpp
@@ -533,7 +533,7 @@ void Battle2::OpponentSprite::ResetAnimFrame(u8 rule)
 		    icn = ICN::CMBTHRON;
 		    switch(rule)
 		    {
-			case OP_IDLE:	animframe_start = 15; animframe_count = 4; break;
+			case OP_IDLE:	animframe_start = 16; animframe_count = 4; break;
 			case OP_SRRW:	animframe_start = 1; animframe_count = 5; break;
 			case OP_CAST:	animframe_start = 6; animframe_count = 9; break;
 			default: break;
